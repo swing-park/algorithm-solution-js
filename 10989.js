6c94049ba90a237759d4dd6994afb70d6769bf11
박정현 <swing@bagjeonghyeon-ui-MacBookPro.local>
2023-06-14 16:07:48 +0900

    Add BOJ 10989 solution

---
+++ 10989.js
@@ -0,0 +1,20 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .slice(1)
+  .map((v) => Number(v));
+
+const max = Math.max(...input);
+const arr = new Array(max + 1).fill(0);
+
+input.forEach((v, i) => {
+  arr[i]++;
+});
+
+arr.forEach((v, i) => {
+  if (v) {
+    arr[i].forEach((x) => console.log(x));
+  }
+});
